36190a73620e3feab67d6a851b726cf363f4956c
fraterpavlo <shukan1999@gmail.com>
2022-07-25 23:56:57 +0300

    feat: begin complite functions n closures tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -23,8 +23,10 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return function func(x) {
+    return f(g(x));
+  };
 }
 
 
@@ -44,8 +46,10 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return function func(x) {
+    return x ** exponent;
+  };
 }
 
 
@@ -81,8 +85,16 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const memory = new Map();
+  return function newFunc(arg) {
+    if (memory.has(arg)) {
+      return memory.get(arg);
+    }
+    const result = func(arg);
+    memory.set(arg, result);
+    return result;
+  };
 }
 
 
@@ -101,7 +113,16 @@ function memoize(/* func */) {
  * }, 2);
  * retryer() => 2
  */
+
 function retry(/* func, attempts */) {
+  // for (let i = 0; i <= attempts; i += 1) {
+  //   try {
+  //     return func();
+  //   } catch (err) {
+  //     if (attempts === 0) return err;
+  //     retry(func, attempts - 1);
+  //   }
+  // }
   throw new Error('Not implemented');
 }
 
@@ -130,6 +151,9 @@ function retry(/* func, attempts */) {
  *
  */
 function logger(/* func, logFunc */) {
+  // return function abc(arg) {
+  //   logFunc(func(arg));
+  // };
   throw new Error('Not implemented');
 }
 
@@ -148,6 +172,9 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
 function partialUsingArguments(/* fn, ...args1 */) {
+  // return function abc(...args) {
+  //   return fn(...args, ...args1);
+  // };
   throw new Error('Not implemented');
 }
 
